f968a9dff43e4a023480856a3d82b7a1d48bd862
Mohammad Abbasi <106863232+mohabbasi1213@users.noreply.github.com>
2024-04-06 14:41:57 +0330

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "AyanTechNetworkingLibrary-iOS",
+            name: "AyanTechNetworkingLibrary",
             targets: ["AyanTechNetworkingLibrary-iOS"]
         ),
     ],
