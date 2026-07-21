c4a3054eedff6eb36adaabd1560cf583323af92f
moh.abbasi <moh.abbasi1213@gmail.com>
2026-07-21 11:16:19 +0330

    feat(demo): add Combine usage examples to demo viewController

--- AyantechNetworkingLibraryDemo/AyantechNetworkingLibraryDemo/ViewController.swift
+++ AyantechNetworkingLibraryDemo/AyantechNetworkingLibraryDemo/ViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import AyanTechNetworkingLibrary
+import Combine
 import UIKit
 
 class ViewController: UIViewController {
     private let apiURL = "YOUR_URL"
+    private var cancellables = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,24 +24,54 @@ class ViewController: UIViewController {
     }
 
     @IBAction private func sendButtonPressed() {
+        // MARK: - Callback (deprecated)
+
         let request = ATRequest.request(url: apiURL, method: .get)
 
         request.send { res in
-            print(res.responseString ?? "null")
+            print("Callback response:", res.responseString ?? "null")
         }
         doWithDelay(2.0) {
             request.cancel()
         }
-        
-        if #available(iOS 13.0, *) {
-            Task {
-                let request = ATRequest.request(url: apiURL, method: .post)
-                let response = await request.send()
+
+        // MARK: - Async/await
+
+        Task {
+            let request = ATRequest.request(url: apiURL, method: .post)
+            let response = await request.send()
+            await MainActor.run {
                 print("Async/await response:", response.responseString ?? "null")
             }
-        } else {
-            print("Async/await requires iOS 13+")
         }
+
+        // MARK: - Combine
+
+        let combineRequest = ATRequest.request(url: apiURL, method: .post)
+        combineRequest.responsePublisher()
+            .receive(on: DispatchQueue.main)
+            .sink { response in
+                if let error = response.error {
+                    print("Combine responsePublisher error:", String(describing: error.type))
+                    return
+                }
+                print("Combine responsePublisher:", response.responseString ?? "null")
+            }
+            .store(in: &cancellables)
+
+        combineRequest.valuePublisher()
+            .receive(on: DispatchQueue.main)
+            .sink(
+                receiveCompletion: { completion in
+                    if case .failure(let error) = completion {
+                        print("Combine valuePublisher failure:", String(describing: error.type))
+                    }
+                },
+                receiveValue: { response in
+                    print("Combine valuePublisher:", response.responseString ?? "null")
+                }
+            )
+            .store(in: &cancellables)
     }
 }
 
